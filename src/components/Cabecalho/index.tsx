40e44e3b11bf01543487d6e3ba53fce28b3a9891
valter <valter@teste.com>
2025-02-01 10:54:52 -0300

    estilização do cabeçalho com styled-components

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,26 @@
-import styles from './Cabecalho.module.css'
+// import styles from './Cabecalho.module.css'
 
-const Cabecalho = () => (
-  <header className={styles.cabecalho}>
+// const Cabecalho = () => (
+//   <header className={styles.cabecalho}>
+//     <h1>EBAC Jobs</h1>
+//   </header>
+// )
+
+// export default Cabecalho
+
+import styled from 'styled-components'
+
+export const Cabecalho = styled.header`
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`
+
+const CabecalhoComponent = () => (
+  <Cabecalho>
     <h1>EBAC Jobs</h1>
-  </header>
+  </Cabecalho>
 )
 
-export default Cabecalho
+export default CabecalhoComponent
